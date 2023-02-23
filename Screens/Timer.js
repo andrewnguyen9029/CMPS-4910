c54da22250b3c751164cb876c00dd431b1c49e85
ranbirgrewal63 <ranbirgrewal@DESKTOP-OC9VOHJ.localdomain>
2023-02-22 21:53:48 -0800

    2nd fix

--- Screens/Timer.js
+++ Screens/Timer.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function Stopwatch() {
+export default function Timer() {
   return (
     <View style={styles.container}>
       <Text>Timer Screen</Text>
