4eb616ad6baf4d8f07600ed05115625899c3dfff
michaelabad2525 <119697712+michaelabad2525@users.noreply.github.com>
2023-02-28 22:30:44 -0800

    check the bottom comments

--- Screens/Alarm.js
+++ Screens/Alarm.js
@@ -19,16 +19,52 @@ export default function Alarm() {
   const handleTimeConfirm = (date) => {
     //console.warn("Time Selected: ", date);
     const dt = new Date(date);
-    const x = dt.toLocaleTimeString(); 
+    //const x = dt.toLocaleTimeString('en-US'); 
+    const x = `${dt.getHours()}:${dt.getMinutes()}:0`;
     console.log(x);
     setSelectedTime(x);
 
     hideTimePicker();
+    //MA
+    alarm = `${dt.getHours()}:${dt.getMinutes()}:0`;
+    console.log(`Current time is ${clock}`);
+    alarmSet = true;
+    console.log(`alarmSet is ${alarmSet}`);
+    console.log(`alarm is set for ${alarm}`);
+    checker();
+    //ME
   };
+  //MA
+  const checker = () => {
+    //checks the current time and compares to time set for alarm
+    console.log(`inside checker`)
+    console.log(`clock: ${clock} alarm: ${alarm}`);
+    let i = 0;
+    while(alarmSet) {
+      today = new Date();
+      clock = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+      console.log(`clock = ${clock}`);
+      if(clock==alarm) alarmSet=false;
+    };
+    ringer();
+  }
 
+  const ringer = () => {
+    //the "alarm" that "rings" once checker is finished
+    console.log(`inside ringer`)  
+    alert(`ring ring`);
+    //obstacles would go around here
+  }
+
+  //variables in use
+  let today = new Date();
+  let clock = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  let alarmSet = false;
+  let alarm = `0`;
+  //ME
   return (
+    
     <View style={styles.container}>
-
       <TouchableOpacity
         style = {{
           width: '50%', 
@@ -40,7 +76,10 @@ export default function Alarm() {
           alignItems: 'center', 
           }}
            onPress={() =>{
+            //console.log(`before timePicker`);
             showTimePicker();
+            //console.log(`before checker`);
+            //setTimeout(checker(),1000);
           }}>
            <Text style={styles.setFontSizeZero}>Select Alarm</Text>
           <DateTimePickerModal
@@ -76,4 +115,20 @@ const styles = StyleSheet.create({
 /* Andrew Nguyen
 Imported the date and time picker 
 created const variable for the time selected from the user 
+*/ 
+
+/* Michael Abad
+created checker and ringer functions
+created variables used for checker and ringer functions
+***IMPORTANT***
+code works but 3 (THREE) non-fatal problems that arise because of code
+1. displayed chosen alarm MIGHT not update until after checker and ringer function run
+    1.a. does not always update even after functions are resolved
+    1.b. may display A previous selected time, not necessarily THE previously selected time
+2. app is NOT interactable with while checker function is running
+3. app is NOT IMMEDIATELY interactable with after alarm rings
+    3.a. app is ONLY interactable with AFTER PayloadTooLargeError occurs
+    3.b. app works as intended after error occurs
+    3.c. approximately 1 min 30 secs from alert() until error occurs
+    3.d. error can be seen in logs
 */
